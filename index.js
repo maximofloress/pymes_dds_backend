f62ea3b5cf9ff7c8b1ff5c52d4504c067a63caed
maximofloress <95429@sistemas.frc.utn.edu.ar>
2024-10-27 21:30:58 -0300

    Router articulos y articulosFamilias implementados

--- index.js
+++ index.js
@@ -1,16 +1,19 @@
 const express = require('express');
 const app = express();
-
+const articulosFamiliaRouter = require("./routes/articulosFamiliaRouter");
+const articulosRouter = require("./routes/articulosRouter");
 
 app.use(express.json()); 
 require("./base-orm/sqlite-init");
+app.use(articulosFamiliaRouter);
+app.use(articulosRouter);
 
 app.get('/', (req, res) => {
-    res.send('Hello World!');
+    res.send('API backend de Pymes corriendo...');
     });
 
 const port = 3000
 app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
+    console.log(`Servidor corriendo en http://localhost:${port}`);
     });
 
